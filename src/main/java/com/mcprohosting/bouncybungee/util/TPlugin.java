9316a98b44157d71af44a86b5567be2f2337b0ce
Joey Sacchini <joey.sacchini264@gmail.com>
2013-11-20 02:13:45 -0500

    Removed some un-needed fat

--- src/main/java/com/mcprohosting/bouncybungee/util/TPlugin.java
+++ src/main/java/com/mcprohosting/bouncybungee/util/TPlugin.java
@@ -1,7 +1,5 @@
 package com.mcprohosting.bouncybungee.util;
 
-import com.mcprohosting.bouncybungee.util.command.TCommandDispatch;
-import com.mcprohosting.bouncybungee.util.command.TCommandHandler;
 import net.md_5.bungee.api.plugin.Listener;
 import net.md_5.bungee.api.plugin.Plugin;
 
@@ -13,21 +11,13 @@ import net.md_5.bungee.api.plugin.Plugin;
  * To change this template use File | Settings | File Templates.
  */
 public abstract class TPlugin extends Plugin {
-    private TCommandDispatch commandDispatch;
     @Override
     public void onEnable() {
-        if (this instanceof TDatabaseManager) {
-            ((TDatabaseManager)this).preLoad();
-        }
-        this.commandDispatch = new TCommandDispatch(this);
         this.start();
     }
     @Override
     public void onDisable() {
         this.stop();
-    }
-    protected void registerCommandHandler(TCommandHandler handler) {
-        this.commandDispatch.registerHandler(handler);
     }
     protected void registerEvents(Listener listener) {
         getProxy().getPluginManager().registerListener(this, listener);

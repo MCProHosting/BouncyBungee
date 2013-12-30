b29ed9d716533eaf50585e17379abf5eca9ad2f6
Evan Lindsay <evanlindsay@live.com>
2013-12-30 11:28:28 -0800

    Command Validation for GTransfer

--- src/main/java/com/mcprohosting/bouncybungee/igcommand/GTransfer.java
+++ src/main/java/com/mcprohosting/bouncybungee/igcommand/GTransfer.java
@@ -5,6 +5,7 @@ import com.mcprohosting.bouncybungee.command.NetCommand;
 import net.md_5.bungee.api.ChatColor;
 import net.md_5.bungee.api.CommandSender;
 import net.md_5.bungee.api.ProxyServer;
+import net.md_5.bungee.api.config.ServerInfo;
 import net.md_5.bungee.api.connection.ProxiedPlayer;
 import net.md_5.bungee.api.connection.Server;
 import net.md_5.bungee.api.plugin.Command;
@@ -19,6 +20,23 @@ public class GTransfer extends Command {
     public void execute(CommandSender sender, String[] strings) {
         if (!sender.hasPermission("ssundee.transfer")) {
             sender.sendMessage(ChatColor.RED + "You can't perform this command!");
+            return;
+        }
+
+        if (strings.length != 1) {
+            sender.sendMessage(ChatColor.RED + "You must specify a server to teleport players to!");
+            return;
+        }
+
+        boolean found = false;
+        for (ServerInfo info : ProxyServer.getInstance().getServers().values()) {
+            if (info.getName().equalsIgnoreCase(strings[0])) {
+                found = true;
+            }
+        }
+
+        if (found == false) {
+            sender.sendMessage(ChatColor.RED + "That is not a valid server.");
             return;
         }
 
